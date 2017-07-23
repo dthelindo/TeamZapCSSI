1b6215f13de1f51299ae8c6970294fda985a1d6a
Daniel Lindo <dilindo28@gmail.com>
2017-07-23 16:58:00 -0700

    Began main handler, gets currnt user

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #TODO Dainel: Create login, handlers, etc
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import datetime
+import jinja2
+import json
+import logging
+import os
+import urllib
+import urllib2
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        cur_user = users.get_current_user()
+        log_url = ''
+        if cur_user:
+            log_url = users.create_logout_url('/')
+        else:
+            log_url = users.create_login_url('/')
+        search_term = self.request.get('q')
+
+        template = env.get_template('main.html')
